26352ddbebf0c8f3135b3b9f30de77e22b54ae9a
Shalabh Neema <shalabh.neema@sjsu.edu>
2019-12-04 10:21:36 -0800

    update

--- recommendationsystem.py
+++ recommendationsystem.py
@@ -42,6 +42,7 @@ def encoding(data):
 data.select_dtypes(include=['object']).columns
 data = encoding(data)
 
+data['title']
 # for i in range(data.shape[0]):
 #     data['title']=i
 for i in range(data.shape[0]):
